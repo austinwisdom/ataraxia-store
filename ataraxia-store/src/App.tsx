c81787a895e3ef6151374bfa30534813de67182e
Austin <ataraxia@Austins-MacBook-Air.local>
2023-07-13 18:06:06 -0300

    Added fonts and typewriter effect.

--- ataraxia-store/src/App.tsx
+++ ataraxia-store/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
+import './App.scss'
 import Homepage from './pages/Homepage'
 
 function App() {
--- ataraxia-store/src/pages/Homepage.tsx
+++ ataraxia-store/src/pages/Homepage.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom"
-import { useState } from "react"
+import { TypeAnimation } from 'react-type-animation';
+
 
 import headerBanner from "../assets/images/brand/bannerImage.png"
 import computer from "../assets/images/ai-images/computer.png"
@@ -7,33 +8,49 @@ import computer from "../assets/images/ai-images/computer.png"
 import "./Homepage.scss"
 
 const Homepage = () => {
-    const [count, setCount] = useState(0)
-
+    
     return (
-    <main className="header">
+    <div className="header">
       <div className="header__div">
         <img className="header__image" src={headerBanner} alt="Ataraxia banner image" />
       </div>
       <section>
       <nav className="nav">
         <ul className="nav__ul">
-          <NavLink classname="nav__link"><li className="nav__li">トップページ</li></NavLink>
           <NavLink classname="nav__link"><li className="nav__li">ギャラリー</li></NavLink>
           <NavLink classname="nav__link"><li className="nav__li">プロダクツ</li></NavLink>
-          <NavLink classname="nav__link"><li className="nav__li">&#123;  ATARAXIA  &#125;</li></NavLink>
+          <NavLink classname="nav__link">
+            <ul>
+              <li className="nav__li">&#123;  ATARAXIA  &#125;</li>
+              <li className="nav__li nav__li--subtitle">トップページ</li>
+            </ul>
+          </NavLink>
         </ul>
       </nav>
-      <main>
+      <main className="main">
       <img className="main__image" src={computer} alt="Ataraxia banner image" />
-      <iframe className="main__iframe" src="https://open.spotify.com/embed/playlist/1YIe34rcmLjCYpY9wJoM2p?utm_source=generator" width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      </main>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="main__text">
+        <div className="main__conversation">
+        <TypeAnimation
+          style={{ whiteSpace: 'pre-line', height: '195px', display: 'block' }}
+          sequence={[
+            `I am creating art with the machine.\nI am the machine.\nI am the art.`,
+          ]}
+          repeat={0}
+        />
+          {/* <h3>I am creating art with the machine.</h3> */}
+          {/* <h3>I am the machine.</h3>
+          <h3>I am the art.</h3> */}
+          <h3>Clock goes here</h3>
+        </div>
+        <div>
+          Music goes here.
+          {/* <iframe className="main__iframe" width="100%" height="166" scrolling="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1501426021&color=%23ff0099&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div><a href="https://soundcloud.com/lofi_girl" title="Lofi Girl" target="_blank">Lofi Girl</a> · <a href="https://soundcloud.com/lofi_girl/1-am-chill-session" title="1 A.M Chill Session" target="_blank">1 A.M Chill Session</a></div> */}
+        </div>
       </div>
+      </main>
       </section>
-    </main>
+    </div>
     )
     
 }
