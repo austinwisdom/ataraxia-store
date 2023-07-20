1aa2bd4f3e5ee163aef14dadabe63abc097ae2a2
Austin <ataraxia@Austins-MacBook-Air.local>
2023-07-20 18:02:36 -0300

    Added first translation animation and photo on hover animations.

--- ataraxia-store/src/pages/GalleryPage/GalleryPage.tsx
+++ ataraxia-store/src/pages/GalleryPage/GalleryPage.tsx
@@ -1,6 +1,10 @@
 import { FC } from "react";
-import testImage from "../../assets/images/ai-images/computer2.png"
 
+import { TypeAnimation } from 'react-type-animation';
+
+import tv from "../../assets/images/ai-images/computer2.png"
+import garden from "../../assets/images/ai-images/garden.png"
+import cy from "../../assets/images/ai-images/cy.png"
 import "./GalleryPage.scss"
 
 
@@ -8,7 +12,35 @@ const GalleryPage: FC = () => {
     return (
         <div>
             <main className="gallery">
-            <img className="gallery__image" src={testImage} />
+                <div className="gallery__div--image">
+                    <img className="d-1 gallery__image" src={tv} />
+                <div className="text__transform">
+                    <TypeAnimation className="gallery__typewriter"
+                        style={{ whiteSpace: 'pre-line', height: '195px', display: 'block' }}
+                        sequence={[
+                          `これはテストです`,
+                          5000,
+                          `THISはテストです`,
+                          200,
+                          `THISはTESTです`,
+                          200,
+                          `THIS IS A TEST`,
+                          5000
+                        ]}
+                        speed={99}
+                        omitDeletionAnimation={true}
+                        repeat={0}
+                        cursor={false}
+                    />
+                </div>
+                </div>
+                <div className="gallery__div--image">
+                    <img className="d-1 gallery__image" src={garden} />
+                </div>
+                <div className="gallery__div--image">
+                    <img className="d-1 gallery__image" src={cy} />
+                </div>
+
             </main>
         </div>
     );
